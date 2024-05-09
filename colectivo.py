cd4009e1c0f1ecc9256f7241e0b605a1c9e3ae5e
AgustinNespral <agusnespral@gmail.com>
2024-05-09 12:22:15 -0300

    first commit with integer calculator

---
+++ colectivo.py
@@ -0,0 +1,24 @@
+mensaje_bienvenida = ("Bienvenido a la linea de colectivos 21. \nAl momento contamos con tres tramos: \n"
+                      "TRAMO A: $4.25\n"
+                      "TRAMO B: $8.75\n"
+                      "TRAMO C: $12.50")
+
+print(mensaje_bienvenida)
+
+tramo = input('Ingrese la letra del tramo que desea realizar')
+
+print(tramo)
+
+if tramo == 'a':
+    input('Cuantos boletos desea sacar para tramo ' + A)
+
+
+#if paa validar
+#ingree cantidad de boletos que desea para el tramo seleccionado
+#if cantidad boletos <= 10  #desea comprar mas boletos else
+#if yes volver al input
+#else input ingrese las monedas. de que valor es la moneda que va a insertar. la maquina acepta monedas de 0.05, 0.10, 0.25, 0.50, 1, 2, 5, 10
+#cuantas monedas de {valor} desea insertar
+#print cuanto ingreso
+#if dinero ingresado mayor costo de pasajes imprimir resultado de boletos para cada tramo y vuelto
+#else Enhorabuena usted ha comprado tantos pasajes de cada tramo, su vuelto es de... buen viaje
