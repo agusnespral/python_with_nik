c30d67e532868552bec953c9cfc347856e3d360e
nespralnic <niconespral@gmail.com>
2024-05-10 19:44:29 -0300

    refactor de ahorcado con funciones

--- 2.ahorcado.py
+++ 2.ahorcado.py
@@ -4,65 +4,101 @@ words = ["camino", "cochera", "aturdido", "galleta", "barcos", "cadena", "ventan
 
 again = 'y'
 
-while again == 'y':
-    print("\nHello! This game is called 'Ahorcado'. You have to guess the secret word. \n"
-          "You have 7 chances. In each of them, you can either try to guess a single letter or the entire word \n"
-          "If the letter is on the word, its position will be shown.\n"
-          "You must guess the entire word before attemps are over.\n"
-          "Try to get the complete word in as few attemps as possible.")
+def formarBarWord():
+    global chosen_word, bar_word
+    for x in chosen_word:
+        bar_word += '_'
 
-    input("Press ENTER to start")
+def mensajePrimerIntento():
+    global bar_word, esPrimerIntento, attempts_count
+    print("\nYour word is:")
+    print(bar_word, " || ", " 0 characters guessed over", len(bar_word))
+    print("Attempts left: ", attempts_count)
+    esPrimerIntento = False
+
+def arriesgaPalabra():
+    global guessed, chosen_word, chance, attempts, attempts_count
+    if chance == chosen_word:
+        print("CONGRATULATIONS, YOU are the MESSI of the ahorcados! You win the game in", attempts, "attempt/s.\n ")
+        opcionReplay()
+        guessed = True
+    else:
+        print("Bad call! The guessed word is not the secret one. You still have", attempts_count, "attempts.")
+
+def arriesgaLetra():
+    global bar_word, attempts_count
+    pos = 0
+    printing = False
+    for i in chosen_word:
+        if i == chance:
+            bar_word = bar_word[:pos] + i + bar_word[pos + 1:]
+            printing = True
+        pos += 1
+
+    if not printing:
+        if attempts_count > 0:
+            print("Bad call! The guessed letter is not part of the secret word.")
+        else:
+            print("Bad call and thats all.")
+            
+    else:
+        if attempts_count > 0:
+            print("Nice, your letter is in the word!\n", bar_word, ".\n")
+        else:
+            print("Your letter is in the word.")
+
+def opcionReplay():
+    replay = input("Press 'y' if you want to try to guess a new word")
+    if replay == 'y':
+        play()
+    else:
+        despedida()
+
+def despedida():
+    print("\nSayonara!!!.\n")
+
+def play():
+
+    global words, chosen_word, attempts_count, bar_word, chance
 
     attempts = 0
-
     random_num = (random.randint(0, 9))
     chosen_word = words[random_num]
-    char_guessed = 0
-    chosen_word_len = len(chosen_word)
     attempts_count = 7
     bar_word = ""
-    bar_word_count = 0
+    esPrimerIntento = True
     guessed = False
 
-    while attempts < 7:
+    formarBarWord()
 
-        if bar_word_count == 0:
-            print("Your word is:")
-            for x in chosen_word:
-                bar_word += '_'
-            print(bar_word, " || ", char_guessed, "characters guessed over", chosen_word_len)
-            print("Attempts left: ", attempts_count)
-            bar_word_count = 1
+    while attempts < 7 and not guessed:
 
+        if esPrimerIntento:
+            mensajePrimerIntento()
+            
         chance = input("\nPlease, type a letter or an entire word: ")
         attempts += 1
         attempts_count = 7 - attempts
 
-        chance_len = len(chance)
-        if chance_len > 1:
-           if chance == chosen_word:
-                print("CONGRATULATIONS, YOU are the MESSI of the ahorcados! You win the game in", attempts, "attempt/s.\n ")
-                guessed = True
-                again = input("Press 'y' if you want to try to guess a new word or 'e' to exit")
-                break
-           else:
-               print("Bad call! The guessed word is not the secret one. You still have", attempts_count, "attempts.")
+        if len(chance) > 1:
+           arriesgaPalabra()
 
-        elif chance_len == 1:
-            pos = 0
-            printing = 0
-            for i in chosen_word:
-                if i == chance:
-                    bar_word = bar_word[:pos] + i + bar_word[pos + 1:]
-                    printing += 1
-                pos += 1
+        elif len(chance) == 1:
+            arriesgaLetra()
 
-            if printing == 0:
-                print("Bad call! The guessed letter is not part of the secret word. You still have", attempts_count,
-                      "attempts.")
-            else:
-                print("Nice, your letter is in the word!\n", bar_word, ".\n You still have", attempts_count, "attempts.")
+    if not guessed:
+        print("\nBeautiful LOOSER! You have no more attempts left.")
+        opcionReplay()
 
-    if attempts == 7 and guessed == False:
-        print("Beautiful LOOSER! You have no more attempts left.")
-        again = input("Press 'y' if you want to try to guess a new word")
+
+#FLUJO MAIN
+
+print("\nHello! This game is called 'Ahorcado'. You have to guess the secret word. \n"
+        "You have 7 chances. In each of them, you can either try to guess a single letter or the entire word \n"
+        "If the letter is on the word, its position will be shown.\n"
+        "You must guess the entire word before attemps are over.\n"
+        "Try to get the complete word in as few attemps as possible.\n")
+
+input("Press ENTER to start")
+
+play()
